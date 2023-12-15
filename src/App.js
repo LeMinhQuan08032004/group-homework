16d61c933c2556daa5df32b5f2f67332034381b7
LeMinhQuan08032004 <119092949+LeMinhQuan08032004@users.noreply.github.com>
2023-12-15 10:49:38 +0700

    update mới

--- src/App.js
+++ src/App.js
@@ -6,8 +6,7 @@ import Login from './container/auth/login';
 function App() {
   return (
     <div>
-      <h1>Ứng Dụng React Đăng Nhập</h1>
-      <h2>hello</h2>
+      
       <Login />
     </div>
   );
